076dfcc49f96a90bc14e4138f6a8e2e4077cdfaf
Tobias Boogh <tobias.boogh@bybrick.se>
2014-05-14 10:54:41 +0200

    Renamed folder, fixed BOOImageCache UIImage categories

---
+++ BOOiOSGoodies/Categories/UIImage+TINImageCache.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+TINImageCache.h
+//  Catalog
+//
+//  Created by Tobias Boogh on 12/05/14.
+//  Copyright (c) 2014 Tobias Boogh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BOOImageCache.h"
+@interface UIImage (BOOImageCache)
++(void)thumbnailForImageWithFilepath:(NSString *)filePath withSize:(CGSize)size withCompletion:(BOOImageCacheCompletion)completion;
++(void)thumbnailForPdfWithFilepath:(NSString *)filePath withSize:(CGSize)size withCompletion:(BOOImageCacheCompletion)completion;
++(void)thumbnailForVideoWithFilepath:(NSString *)filePath withSize:(CGSize)size withCompletion:(BOOImageCacheCompletion)completion;
+@end
---
+++ BOOiOSGoodies/Categories/UIImage+TINImageCache.m
@@ -0,0 +1,36 @@
+//
+//  UIImage+TINImageCache.m
+//  Catalog
+//
+//  Created by Tobias Boogh on 12/05/14.
+//  Copyright (c) 2014 Tobias Boogh. All rights reserved.
+//
+
+#import "UIImage+TINImageCache.h"
+
+@implementation UIImage (TINImageCache)
++(void)thumbnailForImageWithFilepath:(NSString *)filePath withSize:(CGSize)size withCompletion:(BOOImageCacheCompletion)completion{
+    [[BOOImageCache getInstance] getThumbnailForImage:filePath withSize:size withCompletion:^(UIImage *image) {
+        if(completion){
+            completion(image);
+        }
+    }];
+}
+
++(void)thumbnailForPdfWithFilepath:(NSString *)filePath withSize:(CGSize)size withCompletion:(BOOImageCacheCompletion)completion{
+    [[BOOImageCache getInstance] getThumbnailForPdf:filePath withSize:size withCompletion:^(UIImage *image) {
+        if(completion){
+            completion(image);
+        }
+    }];
+}
+
++(void)thumbnailForVideoWithFilepath:(NSString *)filePath withSize:(CGSize)size withCompletion:(BOOImageCacheCompletion)completion{
+    [[BOOImageCache getInstance] getThumbnailForVideo:filePath withSize:size withCompletion:^(UIImage *image) {
+        if(completion){
+            completion(image);
+        }
+    }];
+}
+
+@end
