f544bbbec4c804f97b28b16d2c1d7b8bbff0791a
Tobias Boogh <tobias.boogh@bybrick.se>
2014-10-08 15:28:28 +0200

    Added sticky header option

--- BOOiOSGoodies/UICollectionViewLayouts/BOOHorizontalCollectionViewLayout.h
+++ BOOiOSGoodies/UICollectionViewLayouts/BOOHorizontalCollectionViewLayout.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface BOOHorizontalCollectionViewLayout : UICollectionViewFlowLayout
-
+@property (nonatomic) BOOL stickHeader;
 @end
--- BOOiOSGoodies/UICollectionViewLayouts/BOOHorizontalCollectionViewLayout.m
+++ BOOiOSGoodies/UICollectionViewLayouts/BOOHorizontalCollectionViewLayout.m
@@ -16,4 +16,41 @@
     }
     return self;
 }
+
+-(UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    return [super layoutAttributesForItemAtIndexPath:indexPath];
+}
+
+-(NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{
+    NSLog(@"%s %@", __PRETTY_FUNCTION__, NSStringFromCGRect(rect));
+    NSArray *attributes = [super layoutAttributesForElementsInRect:rect];
+    BOOL foundHeader = NO;
+    for (UICollectionViewLayoutAttributes *attribute in attributes){
+        if ([attribute.representedElementKind isEqualToString:UICollectionElementKindSectionHeader]){
+            [self modifyAttribute:attribute];
+            foundHeader = YES;
+        }
+    }
+    if (!foundHeader && self.stickHeader){
+        UICollectionViewLayoutAttributes *attribute = [self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader atIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+        [self modifyAttribute:attribute];
+        NSMutableArray *array = [[NSMutableArray alloc] initWithArray:attributes];
+        [array addObject:attribute];
+        attributes = array;
+    }
+    return attributes;
+}
+
+-(BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds{
+    return YES;
+}
+
+-(void)modifyAttribute:(UICollectionViewLayoutAttributes *)attribute{
+    CGRect frame = attribute.frame;
+    frame.origin.x = self.collectionView.contentOffset.x;
+    attribute.frame = frame;
+    attribute.zIndex = NSIntegerMax;
+//    NSLog(@"%@", attribute);
+}
 @end
